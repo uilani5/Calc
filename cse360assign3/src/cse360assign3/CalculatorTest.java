a326ad381f18bf80048aaea2cacee7d520992e05
de_best <de_best@192.168.0.2>
2016-02-23 15:40:08 -0700

    next change

--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -6,39 +6,59 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+	Calculator calc = new Calculator();
+	int total = 0;
+	String aString = "boy";
+	
 	
 	@Test
 	public void testCalculator() {
-		assertNotNull(new Calculator());
+		assertNotNull(calc);
 	}
 
 	@Test
 	public void testGetTotal() {
-		fail("Not yet implemented");
+		total = calc.getTotal();
+		assertTrue(total == 0);
 	}
 	
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		calc.add(50);
+		total = calc.getTotal();
+		assertTrue(total == 50);
 	}
 	
 	@Test
 	public void testSubtract() {
-		fail("Not yet implemented");
+		calc.subtract(10);
+		total = calc.getTotal();
+		assertTrue(total == -10);
 	}
 	
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		calc.add(100);
+		calc.multiply(2);
+		total = calc.getTotal();
+		assertTrue(total == 200);
 	}
 	
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		calc.add(100);
+		calc.divide(4);
+		total = calc.getTotal();
+		assertTrue(total == 25);
+		calc.add(100);
+		calc.divide(0);
+		total = calc.getTotal();
+		assertTrue(total == 0);
 	}
 	
 	@Test
 	public void testGetHistory() {
-		fail("Not yet implemented");
+		aString = calc.getHistory();
+		assertTrue(aString == "");
 	}
 }
