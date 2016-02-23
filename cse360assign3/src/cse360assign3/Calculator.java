17ddf89d1ce92e08c2f53dc64f86f41df5d3f981
de_best <de_best@192.168.0.2>
2016-02-22 20:44:53 -0700

    initial commit

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,34 @@
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
